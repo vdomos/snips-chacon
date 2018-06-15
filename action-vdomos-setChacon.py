6593d5632e95b5965698fc5bbc7cadc9e66d4955
vdomos <domos.vesta@gmail.com>
2018-06-15 23:30:19 +0200

    Replace the two action files in one

--- action-vdomos-setChacon.py
+++ action-vdomos-setChacon.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+"""
+intent message hermes/intent/vdomos:setChaconOff: {"sessionId":"19b22b6f-4dcc-459c-88ef-fc7a26f3fe90","customData":null,"siteId":"default","input":"éteint la lumière du séjour","intent":{"intentName":"vdomos:setChaconOff","probability":1.0},"slots":[{"rawValue":"séjour","value":{"kind":"Custom","value":"séjour"},"range":{"start":21,"end":27},"entity":"room","slotName":"house_room"}]}
+"""
+
 import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
@@ -10,7 +14,7 @@ import requests
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-ROOMSID = {"salon": "5", "canapé": "6", "séjour": "7", "cuisine": "8"}
+DMGROOMSID = {"salon": "5", "canapé": "6", "séjour": "7", "cuisine": "8", "extérieur": "9", "devant": "9", "panier": "10", "chien": "10"}
 DMGCMDURL = "http://hermes:40406/rest/cmd/id/"
 
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
@@ -32,8 +36,11 @@ def subscribe_intent_callback(hermes, intentMessage):
     action_wrapper(hermes, intentMessage, conf)
 
 
-def httpSetChacon(room):
-    url = DMGCMDURL + ROOMSID[room] + "?state=0"        # "http://hermes:40406/rest/cmd/id/7?state=0"
+def httpSetChacon(room, roomState):
+    if room not in DMGROOMSID:
+        print("Erreur, pièce '%s' inconnue" % room)
+        return False
+    url = DMGCMDURL + DMGROOMSID[room] + "?state=" + roomState        # "http://hermes:40406/rest/cmd/id/7?state=1"
     print("url = %s" % url)
     try:
         req = requests.get(url)
@@ -55,19 +62,29 @@ def action_wrapper(hermes, intentMessage, conf):
      
     Refer to the documentation for further details. 
     """
-
+    
+    print("action-vdomos-setChacon.py running ...")
     if len(intentMessage.slots.house_room) > 0:
-        room = intentMessage.slots.house_room.first().value             # We extract the value from the slot "house_room"
-        if httpSetChacon(room):
-            result_sentence = "Lumière {} éteinte".format(str(room))    # The response that will be said out loud by the TTS engine.
+        room = intentMessage.slots.house_room.first().value             # We extract the value from the slot "house_room/room"
+        if intentMessage.intent.intent_name == 'vdomos:setChaconOn':
+            roomState = "1"
         else:
-            result_sentence = "Echec commande lumière {}".format(str(room))            
+            roomState = "0"
+        if httpSetChacon(room, roomState):
+            if roomState == "1":
+                result_sentence = "Lumiere {} allumée".format(str(room))    # The response that will be said out loud by the TTS engine.
+            else:
+                result_sentence = "Lumière {} éteinte".format(str(room)) 
+        else:
+            result_sentence = "Echec commande lumiere {}".format(str(room))            
     else:
         pass
     hermes.publish_end_session(intentMessage.session_id, result_sentence)
     
 
+
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("vdomos:setChaconOff", subscribe_intent_callback) \
-         .start()
+        h.subscribe_intent("vdomos:setChaconOn", subscribe_intent_callback) \
+            .subscribe_intent("vdomos:setChaconOff", subscribe_intent_callback) \
+            .start()
